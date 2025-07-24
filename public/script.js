1e127782aeb4ceb6fdaa66ea9301efc2d074a4d5
Eduardo Nascimento <eduardo.bnascimento2001@gmail.com>
2025-07-23 21:05:22 -0300

    Configura Firebase Hosting e GitHub Actions

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
-function toggleDescription() {
+document.addEventListener("DOMContentLoaded", () => {
+  toggleExperienceDescription();
+  toggleAutomationDescription();
+});
+
+function toggleExperienceDescription() {
   const experienceContainers = document.querySelectorAll(".experience");
   experienceContainers.forEach((container) => {
     container.addEventListener("click", () => {
+      const isExpanded = container.classList.contains("expanded");
       experienceContainers.forEach((otherContainer) => {
         if (otherContainer !== container) {
+          otherContainer.classList.remove("expanded");
           const otherDescriptionSpans =
             otherContainer.querySelectorAll(".descricao-detalhe");
           otherDescriptionSpans.forEach((span) => {
@@ -11,15 +18,45 @@ function toggleDescription() {
           });
         }
       });
+      container.classList.toggle("expanded");
       const currentDescriptionSpans =
         container.querySelectorAll(".descricao-detalhe");
       currentDescriptionSpans.forEach((span) => {
-        span.classList.toggle("visivel");
+        if (isExpanded) {
+          span.classList.remove("visivel");
+        } else {
+          span.classList.add("visivel");
+        }
       });
     });
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  toggleDescription();
-});
+function toggleAutomationDescription() {
+  const automationContainers = document.querySelectorAll(".automacao");
+  automationContainers.forEach((container) => {
+    container.addEventListener("click", () => {
+      const isExpanded = container.classList.contains("expanded");
+      automationContainers.forEach((otherContainer) => {
+        if (otherContainer !== container) {
+          otherContainer.classList.remove("expanded");
+          const otherDescriptionSpans =
+            otherContainer.querySelectorAll(".descricao-detalhe");
+          otherDescriptionSpans.forEach((span) => {
+            span.classList.remove("visivel");
+          });
+        }
+      });
+      container.classList.toggle("expanded");
+      const currentDescriptionSpans =
+        container.querySelectorAll(".descricao-detalhe");
+      currentDescriptionSpans.forEach((span) => {
+        if (isExpanded) {
+          span.classList.remove("visivel");
+        } else {
+          span.classList.add("visivel");
+        }
+      });
+    });
+  });
+}
